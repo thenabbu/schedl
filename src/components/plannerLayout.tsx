6d20aaac282d1a9ae963311233e1e4a3393c7a2b
TheNabbu <navyagupta2871@gmail.com>
2025-05-10 02:54:14 +0530

    fixed grid spacing from right and bottom

--- src/components/plannerLayout.tsx
+++ src/components/plannerLayout.tsx
@@ -8,7 +8,7 @@ export default function PlannerLayout({ children }: { children: React.ReactNode 
         box-border
 
         pt-[1vmin] pl-[1vmin]        /* outer margin top/left */
-        pr-[2vmin] pb-[2vmin]        /* outer margin right/bottom = 1vmin + 2vmin inner gap */
+        pr-[1vmin] pb-[1vmin]        /* outer margin right/bottom = 1vmin + 2vmin inner gap */
       "
     >
       {children}
